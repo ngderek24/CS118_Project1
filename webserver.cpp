f49646f22f73ab680b1710ecf05a93c4887b31ef
tincan24 <tincan24@gmail.com>
2015-10-11 00:53:37 -0700

    Support file request

--- webserver.cpp
+++ webserver.cpp
@@ -7,45 +7,57 @@
 #include <strings.h>
 #include <iostream>
 #include <ctime>
+#include <cstdio>
 
 using namespace std;
 
 void error(string msgString) {
-	const char *msg = msgString.c_str();
+    const char *msg = msgString.c_str();
     perror(msg);
     exit(0);
 }
 
 string getTime(struct tm* tmPtr) {
-	char buffer[100];
-	strftime(buffer, sizeof(buffer), "%a, %d %b %G %T GMT", tmPtr);
-	string dateString(buffer);
-	return dateString;
+    char buffer[100];
+    strftime(buffer, sizeof(buffer), "%a, %d %b %G %T GMT", tmPtr);
+    string dateString(buffer);
+    return dateString;
 }
 
 string getHeaderValue(const string& request, const string& headerName) {
-	string headerValue = "";
-	
-	int headerNameIndex = request.find(headerName);
-	if (headerNameIndex != string::npos) {
-		int headerValueIndex = headerNameIndex + headerName.size() + 2;
-		int carriageReturnIndex = request.find("\r", headerValueIndex);
-		if (carriageReturnIndex != string::npos) {
-			headerValue = request.substr(headerValueIndex, 								carriageReturnIndex - headerValueIndex);
-		}
-	}
-	
-	return headerValue;
+    string headerValue = "";
+    
+    int headerNameIndex = request.find(headerName);
+    if (headerNameIndex != string::npos) {
+        int headerValueIndex = headerNameIndex + headerName.size() + 2;
+        int carriageReturnIndex = request.find("\r", headerValueIndex);
+        if (carriageReturnIndex != string::npos) {
+            headerValue = request.substr(headerValueIndex,                              carriageReturnIndex - headerValueIndex);
+        }
+    }
+    
+    return headerValue;
+}
+
+string getFilename(const string& request) {
+    string filename = "";
+    int filenameIndex = 5;
+    int spaceIndex = request.find(" ", filenameIndex);
+    if (spaceIndex != string::npos) {
+        filename = request.substr(filenameIndex, 
+                    spaceIndex - filenameIndex);
+    } 
+    return filename;
 }
 
 int main(int argc, char *argv[]) {
-	int socketListener, portNum, socketfd;
-	socklen_t clientAddrLength;
-	struct sockaddr_in serverAddr, clientAddr;
-	
-	// open socket
-	socketListener = socket(AF_INET, SOCK_STREAM, 0);
-	if (socketListener < 0) 
+    int socketListener, portNum, socketfd;
+    socklen_t clientAddrLength;
+    struct sockaddr_in serverAddr, clientAddr;
+    
+    // open socket
+    socketListener = socket(AF_INET, SOCK_STREAM, 0);
+    if (socketListener < 0) 
         error("ERROR opening socket");
         
     // zero out bytes of serverAddr and get port number
@@ -54,20 +66,20 @@ int main(int argc, char *argv[]) {
     
     // set serverAddr properties
     serverAddr.sin_family = AF_INET;
-	serverAddr.sin_addr.s_addr = INADDR_ANY;
-	serverAddr.sin_port = htons(portNum);
+    serverAddr.sin_addr.s_addr = INADDR_ANY;
+    serverAddr.sin_port = htons(portNum);
  
- 	// bind socket to IP address and port number
- 	if (bind(socketListener, (struct sockaddr *) &serverAddr, 
- 			sizeof(serverAddr)) < 0) 
- 		error("ERROR on binding");
+    // bind socket to IP address and port number
+    if (bind(socketListener, (struct sockaddr *) &serverAddr, 
+            sizeof(serverAddr)) < 0) 
+        error("ERROR on binding");
  
- 	// listen and accept
- 	listen(socketListener,1);
- 	clientAddrLength = sizeof(clientAddr);
-    socketfd = accept(socketListener, (struct sockaddr *) &clientAddr, 						&clientAddrLength);
+    // listen and accept
+    listen(socketListener,1);
+    clientAddrLength = sizeof(clientAddr);
+    socketfd = accept(socketListener, (struct sockaddr *) &clientAddr,                      &clientAddrLength);
     if (socketfd < 0) 
-    	error("ERROR on accept");
+        error("ERROR on accept");
              
     int result;
     char buffer[1024];
@@ -76,23 +88,45 @@ int main(int argc, char *argv[]) {
     // read client request
     result = read(socketfd, buffer, 1024);
     if (result < 0) 
-    	error("ERROR reading from socket");
+        error("ERROR reading from socket");
     
     // print client request
     cout << buffer << endl;
     
-    // set up response message
-    time_t t;
-    time(&t);
-    struct tm *tmPtr = gmtime(&t);
+    // parse file name
+    const char* filename = getFilename(buffer).c_str();
     
-    // send response to client
-    //res = write(socketfd, ptm->tm_min, 20);
-    //if (res < 0) 
-    	//error("ERROR writing to socket");
+    // open file for reading
+    FILE *fp = fopen(filename, "r");
+    if (fp == NULL) {
+        string errMsg = "404 Not Found";
+        result = write(socketfd, errMsg.c_str(), 30);
+            if (result < 0) 
+                error("ERROR writing to socket");
+        error("ERROR opening file");
+    }
+    else {
+        // write file to buffer
+        char writeBuffer[10000] = { 0 };
+        int bytesRead = fread(writeBuffer, 1, 10000, fp);
+        
+        if (bytesRead > 0) {
+            // send response to client
+            result = write(socketfd, writeBuffer, 10000);
+            if (result < 0) 
+                error("ERROR writing to socket");
+        }
+        else {
+            string errMsg = "500 Internal Server Error";
+            result = write(socketfd, errMsg.c_str(), 30);
+            if (result < 0) 
+                error("ERROR writing to socket");
+        }
+    }
     
-    // close sockets
+    // close sockets and files
+    fclose(fp);
     close(socketfd);         
     close(socketListener);
-	return 0;
+    return 0;
 }
