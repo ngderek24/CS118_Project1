afcccdd6f53abee469836ad774c52f60d1b479bd
Jason Yoon <jasony0206@gmail.com>
2015-10-17 22:38:02 -0700

    Update webserver.cpp

--- webserver.cpp
+++ webserver.cpp
@@ -8,6 +8,7 @@
 #include <iostream>
 #include <ctime>
 #include <cstdio>
+#include <cstring>
 
 using namespace std;
 
@@ -15,28 +16,6 @@ void error(string msgString) {
     const char *msg = msgString.c_str();
     perror(msg);
     exit(0);
-}
-
-string getTime(struct tm* tmPtr) {
-    char buffer[100];
-    strftime(buffer, sizeof(buffer), "%a, %d %b %G %T GMT", tmPtr);
-    string dateString(buffer);
-    return dateString;
-}
-
-string getHeaderValue(const string& request, const string& headerName) {
-    string headerValue = "";
-    
-    int headerNameIndex = request.find(headerName);
-    if (headerNameIndex != string::npos) {
-        int headerValueIndex = headerNameIndex + headerName.size() + 2;
-        int carriageReturnIndex = request.find("\r", headerValueIndex);
-        if (carriageReturnIndex != string::npos) {
-            headerValue = request.substr(headerValueIndex,                              carriageReturnIndex - headerValueIndex);
-        }
-    }
-    
-    return headerValue;
 }
 
 string getFilename(const string& request) {
@@ -50,102 +29,118 @@ string getFilename(const string& request) {
     return filename;
 }
 
-int main(int argc, char *argv[]) {
-    int socketListener, portNum, socketfd;
-    socklen_t clientAddrLength;
-    struct sockaddr_in serverAddr, clientAddr;
-    
+void setup(int &socketListener, int &socketfd, struct sockaddr_in &serverAddr, int portNum) {
     // open socket
     socketListener = socket(AF_INET, SOCK_STREAM, 0);
-    if (socketListener < 0) 
+    if (socketListener < 0)
         error("ERROR opening socket");
-        
-    // zero out bytes of serverAddr and get port number
+    
+    // zero out bytes of serverAddr
     bzero((char *) &serverAddr, sizeof(serverAddr));
-    portNum = atoi(argv[1]);
     
     // set serverAddr properties
     serverAddr.sin_family = AF_INET;
     serverAddr.sin_addr.s_addr = INADDR_ANY;
     serverAddr.sin_port = htons(portNum);
- 
+    
     // bind socket to IP address and port number
-    if (bind(socketListener, (struct sockaddr *) &serverAddr, 
-            sizeof(serverAddr)) < 0) 
+    if (bind(socketListener, (struct sockaddr *) &serverAddr, sizeof(serverAddr)) < 0)
         error("ERROR on binding");
+}
+
+void handleRequest(int socketfd) {
+    int result;
+    char buffer[1024];
+    bzero(buffer, 1024);
+
+    // read client request
+    result = read(socketfd, buffer, 1024);
+    if (result < 0) 
+        error("ERROR reading from socket");
+
+    // print client request
+    cout << buffer << endl;
+
+    // parse file name
+    const char* filename = getFilename(buffer).c_str();
+
+    // if no file specified, just display a message
+    if (strcmp(filename, "") == 0) {
+        result = write(socketfd, "I got your message", 18);
+        if (result < 0) 
+            error("ERROR writing to socket");
+    }
+    else {
+        // open file for reading
+        FILE *fp = fopen(filename, "r");
+        if (fp == NULL) {
+            string errMsg = "404 Not Found";
+            result = write(socketfd, errMsg.c_str(), 30);
+                if (result < 0) 
+                    error("ERROR writing to socket");
+            cout << "ERROR opening file" << endl;
+        }
+        else {
+            // write file to buffer
+            const int BUFFER_SIZE = 2000000;
+            char writeBuffer[BUFFER_SIZE] = { 0 };
+            int bytesRead = fread(writeBuffer, 1, BUFFER_SIZE, fp);
+            
+            if (bytesRead > 0) {
+                // send response to client
+                result = write(socketfd, writeBuffer, BUFFER_SIZE);
+                if (result < 0) 
+                    error("ERROR writing to socket");
+            }
+            else {
+                string errMsg = "500 Internal Server Error";
+                result = write(socketfd, errMsg.c_str(), 30);
+                if (result < 0) 
+                    error("ERROR writing to socket");
+            }
+        }
+        fclose(fp);
+    }
+}
+
+int main(int argc, char *argv[]) {
+    int socketListener, portNum, socketfd;
+    socklen_t clientAddrLength;
+    struct sockaddr_in serverAddr, clientAddr;
+    
+    // get port number and setup
+    portNum = atoi(argv[1]);
+    setup(socketListener, socketfd, serverAddr, portNum);
  
     // listen and accept
     listen(socketListener, 5);
     clientAddrLength = sizeof(clientAddr);
-    int socketNum = 1;
     
-    fd_set active_fd_set;
-    FD_ZERO(&active_fd_set);
-    FD_SET(socketListener, &active_fd_set);
-
     while(1) {
-        /*
-        if (select(socketNum + 1, &active_fd_set, NULL, 
-                    NULL, NULL) < 0)
-            error("ERROR on select");
-        */
-        //cout << "after select";   
-        //if (FD_ISSET(socketListener, &active_fd_set)) {
-            socketfd = accept(socketListener, (struct sockaddr *)                               &clientAddr, &clientAddrLength);
-            if (socketfd < 0) 
-                error("ERROR on accept");
+        socketfd = accept(socketListener, (struct sockaddr *) &clientAddr, &clientAddrLength);
+        if (socketfd < 0) 
+            error("ERROR on accept");
             
-            //socketNum++;
-            //FD_SET(socketfd, &active_fd_set);
-            
-            int result;
-            char buffer[1024];
-            bzero(buffer, 1024);
-        
-            // read client request
-            result = read(socketfd, buffer, 1024);
-            if (result < 0) 
-                error("ERROR reading from socket");
-        
-            // print client request
-            cout << buffer << endl;
-        
-            // parse file name
-            const char* filename = getFilename(buffer).c_str();
-        
-            // open file for reading
-            FILE *fp = fopen(filename, "r");
-            if (fp == NULL) {
-                string errMsg = "404 Not Found";
-                result = write(socketfd, errMsg.c_str(), 30);
-                    if (result < 0) 
-                        error("ERROR writing to socket");
-                error("ERROR opening file");
-            }
-            else {
-                // write file to buffer
-                char writeBuffer[10000] = { 0 };
-                int bytesRead = fread(writeBuffer, 1, 10000, fp);
-                
-                if (bytesRead > 0) {
-                    // send response to client
-                    result = write(socketfd, writeBuffer, 10000);
-                    if (result < 0) 
-                        error("ERROR writing to socket");
-                }
-                else {
-                    string errMsg = "500 Internal Server Error";
-                    result = write(socketfd, errMsg.c_str(), 30);
-                    if (result < 0) 
-                        error("ERROR writing to socket");
-                }
-            }
-            fclose(fp);
-        //}
+        int pid = fork();
+        // child process handles the client request
+        if (pid == 0)  { 
+             close(socketListener);
+             
+             handleRequest(socketfd);
+             exit(0);
+        }
+        // parent closes socket and continues to listen
+        else if (pid > 0) {
+            close(socketfd);
+        }
+        // fork failed
+        else {
+             error("ERROR on fork");
+        }
     }
     
-    // close sockets
-    close(socketfd);  
+    
+    // close sockets 
     close(socketListener);
     return 0;
 }
